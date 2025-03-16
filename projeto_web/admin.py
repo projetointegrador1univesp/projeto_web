d3a3731c2f530900c806faa7105161528ec5d14f
projetointegrador1univesp <23201877@aluno.univesp.br>
2025-03-16 14:02:57 -0300

    Atualizando o projeto

---
+++ projeto_web/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+admin.site.site_header = "Painel de Administração - Projeto Integrador"
+admin.site.site_title = "Administração"
+admin.site.index_title = "Bem-vindo ao Painel Admin"
